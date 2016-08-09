6fcbc9b6894d3a5c3f6012d41765dedfb91352d2
leeight <leeight@gmail.com>
2016-08-09 15:01:49 +0800

    添加 to_hms 的过滤器

--- src/common/adapter/template/etpl.js
+++ src/common/adapter/template/etpl.js
@@ -35,6 +35,23 @@ export default class extends Base {
 
     // FIXME(leeight) 在 production 环境下面，不应该被重复调用，应该只初始化一次
     let engine = new etpl.Engine(options);
+    engine.addFilter('to_hms', function (seconds) {
+      if (seconds < 60) {
+        return '00:' + seconds;
+      }
+
+      let minutes = Math.floor(seconds / 60);
+      seconds = seconds - minutes * 60;
+
+      if (minutes < 60) {
+        return minutes + ':' + seconds;
+      }
+
+      let hours = Math.floor(minutes / 60);
+      minutes = minutes - hours * 60;
+
+      return hours + ':' + minutes + ':' + seconds;
+    });
     let baseTemplateContent = await this.getContent(path.join(options.root_path, 'common', 'base.html')); 
     let pageTemplateContent = await this.getContent(templateFile);
     engine.compile(baseTemplateContent);
